421f602aff02fad75be5e25d563035dadccacd10
xuanruili <63676485+xuanruili@users.noreply.github.com>
2024-02-12 23:18:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-xtebht
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/xuanruili/vue-xtebht)
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!--img alt="Vue logo" src="https://vuejs.org/images/logo.png"-->
+    <ReleaseSummary msg="Start to generate your email here!"/>
+  </div>
+</template>
+
+<script>
+import ReleaseSummary from './components/ReleaseSummary.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ReleaseSummary
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
